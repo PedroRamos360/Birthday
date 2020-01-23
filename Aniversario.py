084af6ab044eef826c194088e555d3220efefdf5
Pedro Ramos <ramosct70@hotmail.com>
2020-01-23 07:33:04 -0300

    Create Aniversario.py

---
+++ Aniversario.py
@@ -0,0 +1,24 @@
+﻿from datetime import date
+
+def verificarAniversario(nome, MesNiver, DiaNiver):
+    dataAtual = date.today()
+    if dataAtual.month == MesNiver and dataAtual.day == DiaNiver:
+        print('Hoje é o Aniversário de {}!'.format(nome))
+        input('')
+
+    elif dataAtual.month == MesNiver and (dataAtual.day + 1) == DiaNiver:
+        print('Amanhã será o Aniversário de {}!'.format(nome))
+        input('')
+
+    elif dataAtual.month == MesNiver and (dataAtual.day + 2) == DiaNiver:
+        print('O Aniversário de {} será em 2 dias!'.format(nome))
+        input('')
+
+    elif dataAtual.month == MesNiver and (dataAtual.day + 3) == DiaNiver:
+        print('O Aniversário de {} será em 3 dias!'.format(nome))
+        input('')
+
+
+
+
+
